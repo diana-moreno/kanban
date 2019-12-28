d160bc73d54740ab850f06e9e809fe65641c6a8d
Diana <d7@hotmail.es>
2019-12-28 20:59:03 +0100

    Updates

---
+++ precache-manifest.c9bcb1adf2bfa13f7bc48dc609509404.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c78d02ad1a71cf734a79ab1a8803673c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "4d8552c0335d661f2d4e",
+    "url": "./static/css/main.789c03a1.chunk.css"
+  },
+  {
+    "revision": "a4ed096121fdba3ef107",
+    "url": "./static/js/2.fecae9bf.chunk.js"
+  },
+  {
+    "revision": "4d8552c0335d661f2d4e",
+    "url": "./static/js/main.cc084902.chunk.js"
+  },
+  {
+    "revision": "07462ac7c24bc8198596",
+    "url": "./static/js/runtime-main.5af3e360.js"
+  },
+  {
+    "revision": "edd6b4c56b6d623ae69067ff70ec6ada",
+    "url": "./static/media/123.edd6b4c5.jpg"
+  }
+]);
