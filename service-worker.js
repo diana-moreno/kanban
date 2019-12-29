2780f6c12796c5f0dde14042b8a3e9db7f30cde4
Diana <d7@hotmail.es>
2019-12-29 20:26:56 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.a560cddc9b6f0989ee2bde17dab8d915.js"
+  "./precache-manifest.1a8a463fb512fa63101ba5d1395209ff.js"
 );
 
 self.addEventListener('message', (event) => {
